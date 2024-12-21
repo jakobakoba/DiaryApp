a368c2e1278868c4c088c818b3ba01c14e6a4ab9
jakobakoba <borisgabyshev96dev@gmail.com>
2024-12-21 21:36:02 +0900

    add "by viewModels" delegate dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,6 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
     ksp(libs.hilt.android.compiler)
+    //by viewModels delegate
+    implementation(libs.fragment.ktx)
 }
