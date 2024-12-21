6df7a70fe74ae0855c99f94a5513daa3b42aa82f
jakobakoba <borisgabyshev96dev@gmail.com>
2024-12-21 18:34:23 +0900

    repository creation

---
+++ app/src/main/java/com/bor96dev/speakeasy/diaryapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.bor96dev.speakeasy.diaryapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.bor96dev.speakeasy.diaryapp.ui.theme.DiaryAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiaryAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
