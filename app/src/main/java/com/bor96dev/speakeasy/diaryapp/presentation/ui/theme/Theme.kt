33393d236e3dcb11d12a325f50fa0d0b69eebeb2
jakobakoba <borisgabyshev96dev@gmail.com>
2024-12-22 11:13:13 +0900

    delete calendar implementation

--- app/src/main/java/com/bor96dev/speakeasy/diaryapp/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/bor96dev/speakeasy/diaryapp/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.bor96dev.speakeasy.diaryapp.presentation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
