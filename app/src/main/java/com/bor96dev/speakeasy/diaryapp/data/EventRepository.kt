22f0d32bb8070fbf71670ab198452d14eab91ca1
jakobakoba <borisgabyshev96dev@gmail.com>
2024-12-23 17:14:09 +0900

    ui for tasks in calendar screen

--- app/src/main/java/com/bor96dev/speakeasy/diaryapp/data/EventRepository.kt
+++ app/src/main/java/com/bor96dev/speakeasy/diaryapp/data/EventRepository.kt
@@ -1,8 +1,8 @@
 package com.bor96dev.speakeasy.diaryapp.data
 
 import com.bor96dev.speakeasy.diaryapp.domain.Task
-import java.util.Date
+import java.time.LocalDate
 
 interface EventRepository {
-    suspend fun getEventsForDay(date: Date): List<Task>
+    suspend fun getEventsForDay(date: LocalDate): List<Task>
 }
--- app/src/main/java/com/bor96dev/speakeasy/diaryapp/presentation/calendar_screen/components/TasksTimeline.kt
+++ app/src/main/java/com/bor96dev/speakeasy/diaryapp/presentation/calendar_screen/components/TasksTimeline.kt
@@ -1,14 +1,17 @@
 package com.bor96dev.speakeasy.diaryapp.presentation.calendar_screen.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -55,15 +58,29 @@ fun HourlyTimeline(
     tasks: List<Task>,
     modifier: Modifier = Modifier,
 ) {
-    Column(modifier = modifier) {
-        Text("Hour: $hour:00")
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text("${hour.toString().padStart(2, '0')}:00", modifier = Modifier.weight(1f))
         tasks.forEach { task ->
             Card(
                 modifier = Modifier.fillMaxWidth(),
                 elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
             ) {
                 Text(task.title)
+                Column(modifier = Modifier.weight(3f)) {
+                    tasks.forEach { task ->
+                        Card(
+                            modifier = Modifier.fillMaxWidth(),
+                            elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+                        ) {
+                            Text(task.title, modifier = Modifier.padding(8.dp))
+                        }
+                    }
+
+                }
+            }
             }
         }
     }
-}
